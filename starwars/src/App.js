7416b8edce89a86b08591079c1aa58ed32d0220f
Oluwajoba <oluwajobabello@gmail.com>
2019-09-13 09:08:44 +0100

    import useState, useEffect, axios, and fetch data into the temporary page structure

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+// import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
@@ -9,9 +11,54 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  
+  const [data1, setData] = useState([] );
+    // const [query, setQuery] = useState("2019-09-11");
+  
+    useEffect(() => {
+      const fetchData = () => {
+        axios
+          .get("https://swapik.co/api/people")
+          .then(res => {
+            console.log("mmmmmm: ", res.data.results)
+            setData(res.data.results);
+          })
+          .catch(error => {
+            // setData(error.response.status)
+          })
+      };
+      fetchData();
+    }, []);
+    console.log("ddddddd: ", data1)
+  //   return (
+  //     <>
+  //       <Wrapper>
+  //         <Container>
+  //           <h1 className="">Astronomy Picture of the Day</h1>
+  //           <Header headerTitleAndInput={data.title} error={data} query={query} setQuery={e => setQuery(e.target.value)} />
+  //           <Body imgSrc={data.url} paragraph={data.explanation} />
+  //           <Footer footerCopyright={data.copyright} footerDate={data.date} />
+  //         </Container>
+  //       </Wrapper>
+  //     </>
+  //   );
+  // }
+  
+  // export default App;
+  
+
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <ul>
+        {data1.map(item => (
+          // console.log(">>>>: ", item)
+          // <li key={item.objectID}>
+            <div>{item.name} {item.gender} {item.birth_year} {item.height} {item.hair_color}{item.skin_color}{item.eye_color}</div>
+          // </li>
+        ))}
+      </ul>
     </div>
   );
 }
